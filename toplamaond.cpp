027c0c8513b65738f60ad2357d8e774f8ccd7b3e
Enes Akdoğan <enesakdogannn@gmail.com>
2025-10-07 12:10:30 +0300

    Create toplamaond.cpp

---
+++ toplamaond.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double first_number, second_number, sum;
+
+    cout << "Enter two numbers: ";
+    cin >> first_number >> second_number;
+
+
+    sum = first_number + second_number;
+
+
+    cout << first_number << " + " << second_number << " = " << sum << endl;
+
+    return 0;
+}
