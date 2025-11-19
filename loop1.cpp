32af411935a488e756bc36a493f07a5e0be1299c
Enes Akdoğan <enesakdogannn@gmail.com>
2025-11-19 11:35:55 +0300

    Create loop1.cpp

---
+++ loop1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+	int a, b, sum;
+	char ans = 'e';
+
+
+	while (ans == 'e' || ans == 'E') {
+		cout << "sayi gir ";
+		cin >> a;
+		cout << "sayi gir ";
+		cin >> b;
+
+		sum = a + b;
+		cout << "toplam: " << sum << endl;
+		cout << "devam etmek istiyor musunuz (e/E) ";
+		cin >> ans;
+
+	}
+
+	cout << "program bitti" << endl;
+}
