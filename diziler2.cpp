7c5b2e822a7fa6d773eb4b155249b15f228b2476
Enes Akdoğan <enesakdogannn@gmail.com>
2025-12-02 12:04:48 +0300

    Add files via upload

---
+++ diziler2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>	
+using namespace std;
+int main() {
+	int dizi[3];
+	cout << "3 sayi gir:";
+	for (int i = 0;i < 3; i++) {
+		
+		cin >> dizi[i];
+
+	}
+	for (int i = 0; i < 3;i++) {
+		cout << dizi[i] << endl;
+	}
+
+	return 0;
+
+
+}
